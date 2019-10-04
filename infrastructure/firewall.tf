d45d5d27d59cc807fcc25c596c23c3d1efd2bf04
Ivanna Terletska <i.terletska1@gmail.com>
2019-10-04 06:36:40 +0300

    Create firewall.tf

---
+++ infrastructure/firewall.tf
@@ -0,0 +1,39 @@
+resource "google_compute_firewall" "externalport-8081" {
+  name    = "${var.javaserver_instance_name}-external-tcp-8081"
+  project = var.project_id
+  network = var.jenkins_instance_network
+
+  allow {
+    protocol = "tcp"
+    ports    = ["8081"]
+  }
+
+  source_ranges           = var.javaserver_instance_access_cidrs
+}
+
+resource "google_compute_firewall" "externalport-443" {
+  name    = "${var.javaserver_instance_name}-external-tcp-443"
+  project = var.project_id
+  network = var.javaserver_instance_network
+
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+
+  source_ranges           = var.javaserver_instance_access_cidrs
+}
+
+
+resource "google_compute_firewall" "externalport-27017" {
+  name    = "mongoserver-${var.mongoserver_instance_name}-external-tcp-443"
+  project = var.project_id
+  network = var.mongoserver_instance_network
+
+  allow {
+    protocol = "tcp"
+    ports    = ["27017"]
+  }
+
+  source_ranges           = var.mongoserver_instance_access_cidrs
+}

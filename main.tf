c52378dc6d355cd1adfade26ac69e4ddcd1f1f10
Ivanna Terletska <i.terletska1@gmail.com>
2019-10-27 12:57:43 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,50 +3,7 @@ provider "google" {
   
 }
 
-  resource "google_compute_network" "servers-network" {
-   project      = var.project_id
-   name = var.network_name
-}
-
-resource "google_compute_firewall" "externalport-8081" {
-  name    = "${var.javaserver_instance_name}-external-tcp-8081"
-  project = var.project_id
-  network = var.javaserver_instance_network
-
-  allow {
-    protocol = "tcp"
-    ports    = ["8081"]
-  }
-
-  source_ranges           = var.javaserver_instance_access_cidrs
-}
-
-resource "google_compute_firewall" "externalport-443" {
-  name    = "${var.javaserver_instance_name}-external-tcp-443"
-  project = var.project_id
-  network = var.javaserver_instance_network
-
-  allow {
-    protocol = "tcp"
-    ports    = ["443"]
-  }
-
-  source_ranges           = var.javaserver_instance_access_cidrs
-}
-
-resource "google_compute_firewall" "externalport-27017" {
-  name    = "mongoserver-${var.mongoserver_instance_name}-external-tcp-443"
-  project = var.project_id
-  network = var.mongoserver_instance_network
-
-  allow {
-    protocol = "tcp"
-    ports    = ["27017"]
-  }
-
-  source_ranges           = var.mongoserver_instance_access_cidrs
-}
-
+ 
 resource "google_compute_instance" "javaserver" {
   project      = var.project_id
   name         = var.javaserver_instance_name

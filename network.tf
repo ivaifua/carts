ba12b18d17c7fa18868d53d8f6e9a69b787b07a0
Ivanna Terletska <i.terletska1@gmail.com>
2019-10-28 00:06:37 +0200

    Delete network.tf

--- network.tf
+++
@@ -1,8 +0,0 @@
-resource "google_compute_network" "servers-network" {
-   project      = var.project_id
-   name = var.instance_network
-   timeouts {
-    create = "90s"
-    delete = "90s"
-  }
-  }

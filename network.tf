b5e08ae12e0278107fba245c3eea0432791362fd
Ivanna Terletska <i.terletska1@gmail.com>
2019-10-27 13:58:40 +0200

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,4 +1,10 @@
 resource "google_compute_network" "servers-network" {
    project      = var.project_id
+   name = var.instance_network
+}
+
+resource "google_compute_subnetwork" "servers-subnetwork" {
+   project      = var.project_id
    name = var.instance_subnetwork
+   network = var.instance_network
 }

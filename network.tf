56f5722ac66372b40080eed29b542a491c7fd1a8
Ivanna Terletska <i.terletska1@gmail.com>
2019-10-27 23:51:14 +0200

    Update network.tf

--- network.tf
+++ network.tf
@@ -2,7 +2,7 @@ resource "google_compute_network" "servers-network" {
    project      = var.project_id
    name = var.mongoserver_instance_network
    timeouts {
-    create = "60s"
-    delete = "60s"
+    create = "90s"
+    delete = "90s"
   }
   }

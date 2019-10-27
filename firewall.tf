e96229a41c43beeb338d2ac7afd3b3633c689667
Ivanna Terletska <i.terletska1@gmail.com>
2019-10-27 23:55:59 +0200

    Update firewall.tf

--- firewall.tf
+++ firewall.tf
@@ -1,7 +1,7 @@
 resource "google_compute_firewall" "externalport-8081" {
   name    = "${var.javaserver_instance_name}-external-tcp-8081"
   project = var.project_id
-  network = var.javaserver_instance_network
+  network = var.instance_network
   allow {
     protocol = "tcp"
     ports    = ["8081"]
@@ -13,7 +13,7 @@ resource "google_compute_firewall" "externalport-8081" {
 resource "google_compute_firewall" "externalport-443" {
   name    = "${var.javaserver_instance_name}-external-tcp-443"
   project = var.project_id
-  network = var.javaserver_instance_network
+  network = var.instance_network
   allow {
     protocol = "tcp"
     ports    = ["443"]
@@ -25,7 +25,7 @@ resource "google_compute_firewall" "externalport-443" {
 resource "google_compute_firewall" "externalport-27017" {
   name    = "mongoserver-${var.mongoserver_instance_name}-external-tcp-443"
   project = var.project_id
-  network = var.mongoserver_instance_network
+  network = var.instance_network
 
   allow {
     protocol = "tcp"

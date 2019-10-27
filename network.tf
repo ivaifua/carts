9124ee64f9ed096dd8ee423844356004cc093f73
Ivanna Terletska <i.terletska1@gmail.com>
2019-10-27 14:39:02 +0200

    Update network.tf

--- network.tf
+++ network.tf
@@ -7,6 +7,7 @@ resource "google_compute_subnetwork" "servers-subnetwork" {
    project      = var.project_id
    name = var.instance_subnetwork
    network = var.instance_network
+   region = var.subnetwork-region
    ip_cidr_range = var.subnetwork-ip_cidr_range
    secondary_ip_range {
      range_name    = var.subnetwork-range_name

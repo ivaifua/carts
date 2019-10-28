f7cabbcafe1d6b566627a249d0182f67b68665a3
Ivanna Terletska <i.terletska1@gmail.com>
2019-10-28 23:13:44 +0200

    Update firewall.tf

--- firewall.tf
+++ firewall.tf
@@ -1,34 +1,37 @@
 resource "google_compute_firewall" "externalport-8081" {
   name    = "${var.javaserver_instance_name}-external-tcp-8081"
   project = var.project_id
-  network = "${google_compute_network.serversnetwork.self_link}"
+  network = "${google_compute_subnetwork.javaserver_network.self_link}"
   allow {
     protocol = "tcp"
     ports    = ["8081"]
   }
-source_ranges           = var.javaserver_instance_access_cidrs
+
+  source_ranges           = var.javaserver_instance_access_cidrs
 }
 
 resource "google_compute_firewall" "externalport-443" {
   name    = "${var.javaserver_instance_name}-external-tcp-443"
   project = var.project_id
-  network = "${google_compute_network.serversnetwork.self_link}"
+  network = "${google_compute_subnetwork.javaserver_network.self_link}"
   allow {
     protocol = "tcp"
     ports    = ["443"]
   }
-source_ranges           = var.javaserver_instance_access_cidrs
+
+  source_ranges           = var.javaserver_instance_access_cidrs
 }
 
 resource "google_compute_firewall" "externalport-27017" {
-  name    = "${var.mongoserver_instance_name}-external-tcp-27017"
+  name    = "mongoserver-${var.mongoserver_instance_name}-external-tcp-443"
   project = var.project_id
-  network = "${google_compute_network.serversnetwork.self_link}"
+  network = "${google_compute_subnetwork.mongoserver_network.self_link}"
   allow {
     protocol = "tcp"
     ports    = ["27017"]
   }
-source_ranges           = var.mongoserver_instance_access_cidrs
+
+  source_ranges           = var.mongoserver_instance_access_cidrs
 }
 
 resource "google_compute_firewall" "externalport-22" {
@@ -39,6 +42,5 @@ resource "google_compute_firewall" "externalport-22" {
     protocol = "tcp"
     ports    = ["22"]
   }
- 
+  
 }
-

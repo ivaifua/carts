9131e8bdd96b4a56269a50c81ed42fa6a5fe4148
Ivanna Terletska <i.terletska1@gmail.com>
2019-10-28 02:08:40 +0200

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
-data "google_compute_network" "server-netvork" {
+data "google_compute_network" "instance_network" {
   name = "server-network"
-  subnetworks_self_links = var.javaserver_instance_network, mongoserver_instance_network
+  subnetworks_self_links = var.javaserver_instance_network, var.mongoserver_instance_network
   timeouts {
     create = "90s"
     delete = "90s"

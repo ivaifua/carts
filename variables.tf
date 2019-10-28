4fea9b5611d7cfcf91d7e2117051c77b8ca25aba
Ivanna Terletska <i.terletska1@gmail.com>
2019-10-28 05:45:56 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -76,12 +76,12 @@ variable "mongoserver_instance_zone" {
 
 variable "mongoserver_instance_network" {
   description = "The network to deploy the mongoserver VM in"
-  default     = "mongoserver_network"
+  default     = "mongoservernetwork"
 }
 
 variable "javaserver_instance_network" {
   description = "The network to deploy the javaserver VM in"
-  default     = "javaserver_network"
+  default     = "javaservernetwork"
 }
 
 variable "instance_network_name" {

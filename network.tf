355922e0e54faca0ba9b8bade6489df3cc9aad4d
Ivanna Terletska <i.terletska1@gmail.com>
2019-10-28 02:23:11 +0200

    Delete network.tf

--- network.tf
+++
@@ -1,8 +0,0 @@
-data "google_compute_network" "instance_network" {
-  name = var.instance_network_name
-  subnetworks_self_links = "${var.javaserver_instance_network}", "${var.mongoserver_instance_network}"
-  timeouts {
-    create = "90s"
-    delete = "90s"
-  }
-}
